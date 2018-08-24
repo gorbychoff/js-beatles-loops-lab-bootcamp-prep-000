514d38fe892e6ec545330ce993cee9285d34d49a
Viam Mercer <viam.mercer@hotmail.com>
2018-08-24 08:03:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var array1 = [];
-  var i = 0;
+  var i;
   for (i = 0; i < musicians.length; i++){
       array1.push(`'${musicians[i]} plays ${instruments[i]}'`);
   }
