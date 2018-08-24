306b4f3fa954c99e494ccc3663cb21bc2c2910a7
Viam Mercer <viam.mercer@hotmail.com>
2018-08-24 08:05:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array1 = [];
   var i;
   for (i = 0; i < musicians.length; i++){
-      array1.push(`'${musicians[i]} plays ${instruments[i]}'`);
+      array1.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array1;
 }
